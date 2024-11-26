de7786619d0387b4558ed12042a1038b5d62a430
BenoitAdam94 <adam050986@yahoo.fr>
2024-11-26 17:15:40 +0100

    update

--- poo_projet1/public/pages/home.php
+++ poo_projet1/public/pages/home.php
@@ -1,5 +1,18 @@
+<?php
+
+$pdo = new PDO('mysql:dbname=courspoo;host=localhost', 'root', '');
+
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+// PDO EXEC
+$count = $pdo->exec('INSERT INTO articles SET titre="Mon Titre", date="' . date('Y-m-d H:i;s').  '"');
+// var_dump($count); // Verification, si 1 = requette executé
+
+$res = $pdo->query("SELECT * FROM articles");
+
+
+?>
 je suis la home
 
 <a href="index.php?p=single">Single PHP</a>
