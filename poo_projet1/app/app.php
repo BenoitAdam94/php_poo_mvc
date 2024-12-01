eb39a0c9cdafac00c00c3a690019a4a8fad6f5cf
BenoitAdam94 <adam050986@yahoo.fr>
2024-12-01 23:26:28 +0100

    add app.php

---
+++ poo_projet1/app/app.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class App
+{
+
+    const DB_NAME = 'courspoo';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+    const DB_HOST = 'localhost';
+
+    private static $database;
+
+    public static function getDatabase()
+    {
+        if (self::$database === null) {
+            self::$database = new App\Database('blog');
+            return self::$database;
+        }
+    }
+}
